40e68a92e9aa2c8509d584be74bee39f7de9a241
Rachapon23 <ohm32070@gmail.com>
2023-05-02 16:31:24 +0700

    Add files via upload

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using HikSDK;
+namespace DQiTsl
+{
+    class Program
+    {
+        static void Main()
+        {
+            CHCNetSDK.NET_DVR_Init();
+            Console.WriteLine(CHCNetSDK.NET_DVR_Init());
+        }
+    }
+}
